a2d81db6baeceb80fd4676e0687adb588f03c791
mosadomi <mosadomipeter@gmail.com>
2021-03-03 03:18:22 +0100

    initial commit

--- types.go
+++ types.go
@@ -1,22 +1,22 @@
 package gonewsapi
 
 type Response struct {
-	Status string `json:"status"`
-	TotalResults int `json:"totalResults"`
-	Articles []Article `json:"articles"`
+	Status       string    `json:"status"`
+	TotalResults int       `json:"totalResults"`
+	Articles     []Article `json:"articles"`
 }
 
 type Article struct {
-	Source []Source `json:"source"`
-	Author string `json:"author"`
-	Title string `json:"title"`
-	Description string `json:"description"`
-	Url string `json:"url"`
-	PublishedAt string `json:"publishedAt"`
-	Content string `json:"content"`
+	Source      []Sourcing `json:"source"`
+	Author      string     `json:"author"`
+	Title       string     `json:"title"`
+	Description string     `json:"description"`
+	Url         string     `json:"url"`
+	PublishedAt string     `json:"publishedAt"`
+	Content     string     `json:"content"`
 }
 
-type Source struct {
-	ID int `json:"id"`
+type Sourcing struct {
+	ID   int    `json:"id"`
 	Name string `json:"name"`
 }
