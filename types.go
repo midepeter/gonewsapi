0e5d2b2bc1fe2f4910bf3bec0d5a0069550cc6e5
mosadomi <mosadomipeter@gmail.com>
2021-03-04 02:38:52 +0100

    initial commit

--- types.go
+++ types.go
@@ -7,16 +7,16 @@ type Response struct {
 }
 
 type Article struct {
-	Source      []Sourcing `json:"source"`
-	Author      string     `json:"author"`
-	Title       string     `json:"title"`
-	Description string     `json:"description"`
-	Url         string     `json:"url"`
-	PublishedAt string     `json:"publishedAt"`
-	Content     string     `json:"content"`
+	Source      Source `json:"source"`
+	Author      string `json:"author"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Url         string `json:"url"`
+	PublishedAt string `json:"publishedAt"`
+	Content     string `json:"content"`
 }
 
-type Sourcing struct {
+type Source struct {
 	ID   int    `json:"id"`
 	Name string `json:"name"`
 }
